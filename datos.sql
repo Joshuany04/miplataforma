bd5ffc11660cc92f14ab47c0be14fc33bd6851cd
Joshuany04 <joshuany.garcia@utelvt.edu.ec>
2022-09-08 14:25:54 -0500

    base de datos

---
+++ datos.sql
@@ -0,0 +1,4 @@
+crete database miplataforma;
+use miplataforma;
+
+create table persona(idpersona int, nombres varchar(20), apellidos varchar(20), cedula varchar(10), idgenero int, fechadenacimiento date, idnacionalidad int, idetnia int, idestadocivil int);
